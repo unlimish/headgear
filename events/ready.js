3ac6c3852229f19fbc32cc45923e031a7ce28db4
unlimish <14168376+unlimish@users.noreply.github.com>
2024-03-20 06:22:31 +0900

    Update dependencies and fix event name

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ const { Events } = require("discord.js");
 const { execute } = require("../commands/utility/weather");
 
 module.exports = {
-  name: Events.ClietnReady,
+  name: Events.ClientReady,
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
--- index.js
+++ index.js
@@ -19,6 +19,7 @@ for (const folder of commandFolders) {
     // Set a new item in the Collection with the key as the command name and the value as the exported module
     if ("data" in command && "execute" in command) {
       client.commands.set(command.data.name, command);
+      console.log(`loaded: ${command.data.name}`);
     } else {
       console.log(
         `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
@@ -27,9 +28,21 @@ for (const folder of commandFolders) {
   }
 }
 
-try {
-  client.login(token);
-  console.log("Bot is now online!");
-} catch (error) {
-  console.log(error);
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+console.log(`loaded: [${eventFiles}]`);
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
+
+client.login(token);
