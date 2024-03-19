d0d3316862ecd90a4604d71c0646de0786f4796c
unlimish <14168376+unlimish@users.noreply.github.com>
2024-03-20 03:51:08 +0900

    Remove weather functionality and update dependencies

---
+++ src/commands/weather.ts
@@ -0,0 +1,12 @@
+import { SlashCommandBuilder } from "discord.js";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("weather")
+    .setDescription("Get the current weather"),
+  async execute(interaction) {
+    await interaction.reply(
+      "uwu ${interaction.guild.name} ${interaction.guild.memberCount}"
+    );
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
-import { Client, Intents, Interaction } from "discord.js";
-import fetch from "node-fetch";
-import * as config from "./config.json";
+import {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  Interaction,
+} from "discord.js";
+import * as config from "../config.json";
+import fs from "node:fs";
+import path from "node:path";
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
 
+const folderPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(folderPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".ts"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `{WARN} The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
 client.once("ready", () => {
   console.log(`Logged in as ${client.user!.tag}!`);
 });
@@ -16,8 +44,11 @@ client.on("interactionCreate", async (interaction: Interaction) => {
     const apiUrl = `https://zutool.jp/api/getweatherstatus/${placeId}`;
 
     try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
+      const fetch = await import("node-fetch");
+      const response = await fetch.default(apiUrl);
+      const responseData = await response.text();
+      const data = JSON.parse(responseData);
+      console.log(data);
 
       let formattedWeather = "";
 
