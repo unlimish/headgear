545f43c8941dd06bb3beb61161c294fd0f8e5c35
unlimish <14168376+unlimish@users.noreply.github.com>
2026-05-22 00:19:04 +0900

    refactor: change ready listener to ClientReady to resolve deprecation warning

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, Collection, GatewayIntentBits, ActivityType } = require("discord.js");
+const { Client, Collection, GatewayIntentBits, ActivityType, Events } = require("discord.js");
 const config = require("./config.json");
 const { token } = config;
 
@@ -93,7 +93,7 @@ async function updatePressureActivity(client) {
   }
 }
 
-client.on("ready", () => {
+client.on(Events.ClientReady, () => {
   updatePressureActivity(client);
   setInterval(() => {
     updatePressureActivity(client);
