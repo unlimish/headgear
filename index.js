31ef1779f811b6bd898beac39f6431966ff3fdbd
unlimish <14168376+unlimish@users.noreply.github.com>
2026-05-21 22:41:07 +0900

    fix: resolve ReferenceError config is not defined in index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require("node:fs");
 const path = require("node:path");
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+const config = require("./config.json");
+const { token } = config;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
